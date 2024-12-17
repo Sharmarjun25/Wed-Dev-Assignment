3b1110902b488d229ff659ca8d9601d6e91eb42d
Arjun Sharma <arjun2508sharma@gmail.com>
2024-12-17 15:29:56 +0530

    Add files via upload

---
+++ Job website/script.js
@@ -0,0 +1,12 @@
+//  Login
+document.getElementById("login-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Login successful!");
+  });
+  
+  // Handle Signup
+  document.getElementById("signup-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Signup successful!");
+  });
+  
